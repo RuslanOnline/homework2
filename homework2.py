1d837f0796ed064cfc5446e7a5d693bfad4a2076
RuslanOnline <scaineton@gmail.com>
2024-07-02 15:57:39 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+first_name_a = "Python"
+apple_count = 12
+second_name_b = (1.5)
+print(second_name_b / apple_count)
+orange_count = 0.125
+print('Курс:',first_name_a,'Всего задач:',apple_count,'Затрачено часов:',second_name_b,'Среднее выполнение:',orange_count)
